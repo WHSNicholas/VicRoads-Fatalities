8cd76e40b5d8f84cdd758d6f72c0a24a3a722523
Nicholas Wong <WHSNicholas@gmail.com>
2024-12-11 21:43:19 +1100

    Adding Data

---
+++ Python Code/1. Exploratory Data Analysis.py
@@ -0,0 +1,84 @@
+# -------------------------------------------------------------------------------------------------------------------- #
+#                                           VicRoads Motor Fatalities Model                                            #
+#                                              Exploratory Data Analysis                                               #
+# -------------------------------------------------------------------------------------------------------------------- #
+
+"""
+Title: VicRoads Motor Fatalities Model
+Script: Exploratory Data Analysis
+
+Authors: Nicholas Wong
+Creation Date: 11th December 2024
+Modification Date: 11th December 2024
+
+Purpose: This script explores the relationship between road fatalities and driver/accident profiles. We use data
+         publicly available from VicRoad to perform data analysis as well as preparing the data for the development of a
+         predictive model using machine learning techniques.
+
+Dependencies: pandas
+
+Instructions:
+
+Data Sources: VicRoad Data obtained from https://discover.data.vic.gov.au/dataset/victoria-road-crash-data
+- Accident Data
+- Vehicle Data
+- Accident Event Data
+- Atmospheric Condition Data
+- Sub DCA Data
+- Person Data
+- Node Data
+- Road Surface Condition Data
+- Accident Location Data
+
+Fonts: "CMU Serif.ttf"
+
+Table of Contents:
+1. Data Integration
+  1.1. Preamble
+  1.2. Importing Data
+2. Data Cleaning
+  2.1. Structuring Data
+  2.2. NA Cleaning
+3. Data Exploration
+4. Data Transformation
+  4.1. Transformation
+  4.2. Dimension Reduction
+"""
+
+# region 1. Data Integration -------------------------------------------------------------------------------------------
+# region 1.1. Preamble -------------------------------------------------------------------------------------------------
+# Required Packages
+import pandas as pd
+
+# endregion
+
+# region 1.2. Importing Data -------------------------------------------------------------------------------------------
+accident = pd.read_csv("../Data/accident.csv")
+
+# endregion
+
+
+# region 2. Data Cleaning ----------------------------------------------------------------------------------------------
+# region 2.1. Structuring Data -----------------------------------------------------------------------------------------
+# endregion
+
+# region 2.2. NA Cleaning ----------------------------------------------------------------------------------------------
+# endregion
+# endregion
+
+# region 3. Data Exploration -------------------------------------------------------------------------------------------
+# endregion
+
+# region 4. Data Transformation ----------------------------------------------------------------------------------------
+# region 4.1. Transformation -------------------------------------------------------------------------------------------
+# endregion
+
+# region 4.2. Dimension Reduction --------------------------------------------------------------------------------------
+# endregion
+# endregion
+
+
+# -------------------------------------------------------------------------------------------------------------------- #
+#                                           VicRoads Motor Fatalities Model                                            #
+#                                              Exploratory Data Analysis                                               #
+# -------------------------------------------------------------------------------------------------------------------- #
