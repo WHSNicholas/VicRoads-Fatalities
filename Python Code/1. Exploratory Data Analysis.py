c12c337d633aa037da84566e0908560e62c2f77e
Nicholas Wong <WHSNicholas@gmail.com>
2024-12-14 11:20:03 +1100

    EDA

--- Python Code/1. Exploratory Data Analysis.py
+++ Python Code/1. Exploratory Data Analysis.py
@@ -82,19 +82,19 @@ dataframes = {
 
 # 2.1. Cleaning Columns --------------------------------------------------------------------------------------
 # Renaming column names
-def snake_case(df):
+def snake_case(dataframe):
     """
     Convert all column names to snake_case.
     """
-    df.columns = (
-        df.columns
+    dataframe.columns = (
+        dataframe.columns
         .str.lower()
         .str.strip()
         .str.replace(' ', '_')
         .str.replace('-', '_')
     )
 
-def rename_columns(df):
+def rename_columns(dataframe):
     """
     Rename some commonly known columns if they exist.
     """
@@ -123,54 +123,55 @@ def rename_columns(df):
     }
 
     for old_col, new_col in rename_dict.items():
-        if old_col in df.columns:
-            df.rename(columns={old_col: new_col}, inplace=True)
+        if old_col in dataframe.columns:
+            dataframe.rename(columns={old_col: new_col}, inplace=True)
 
 for df in dataframes.values():
     snake_case(df)
     rename_columns(df)
 
+del [accident, vehicle, accident_event, atmospheric_cond, sub_dca, person, node, road_surface_cond, accident_location]
 del df
 
 # Dropping columns
-accident.drop(
+dataframes['accident'].drop(
     labels=['accident_type_desc', 'day_week_desc', 'dca_desc', 'no_persons_not_inj', 'road_geometry_desc'],
     axis=1,
     inplace=True
 )
 
-vehicle.drop(
+dataframes['vehicle'].drop(
     labels=['road_surface_type_desc', 'vehicle_power', 'vehicle_type_desc', 'vehicle_colour_1', 'vehicle_colour_2',
             'traffic_control_desc'],
     axis=1,
     inplace=True
 )
 
-accident_event.drop(
+dataframes['accident_event'].drop(
     labels=['event_type_desc', 'vehicle_1_coll_pt_desc', 'vehicle_2_coll_pt_desc', 'person_id', 'object_type_desc'],
     axis=1,
     inplace=True
 )
 
-atmospheric_cond.drop(
+dataframes['atmospheric_cond'].drop(
     labels=['atmosph_cond_seq', 'atmosph_cond_desc'],
     axis=1,
     inplace=True
 )
 
-sub_dca.drop(
+dataframes['sub_dca'].drop(
     labels=['sub_dca_code_desc', 'sub_dca_seq'],
     axis=1,
     inplace=True
 )
 
-person.drop(
+dataframes['person'].drop(
     labels=['inj_level_desc', 'road_user_type_desc'],
     axis=1,
     inplace=True
 )
 
-road_surface_cond.drop(
+dataframes['road_surface_cond'].drop(
     labels=['surface_cond_desc', 'surface_cond_seq'],
     axis=1,
     inplace=True
@@ -181,16 +182,16 @@ road_surface_cond.drop(
 # Creating merged dataframe
 vicroad_df = pd.DataFrame(
     {
-        'id': vehicle['accident_id'].astype(str) + '-' + vehicle['vehicle_id'].astype(str),
-        'accident_id': vehicle['accident_id'],
-        'vehicle_id': vehicle['vehicle_id']
+        'id': dataframes['vehicle']['accident_id'].astype(str) + '-' + dataframes['vehicle']['vehicle_id'].astype(str),
+        'accident_id': dataframes['vehicle']['accident_id'],
+        'vehicle_id': dataframes['vehicle']['vehicle_id']
     }
 )
 
 # One-Hot Encoding
 event_dummies = (
     pd.get_dummies(
-        accident_event[['accident_id', 'event_type', 'vehicle_1_coll_pt', 'vehicle_2_coll_pt']],
+        dataframes['accident_event'][['accident_id', 'event_type', 'vehicle_1_coll_pt', 'vehicle_2_coll_pt']],
         columns=['event_type', 'vehicle_1_coll_pt', 'vehicle_2_coll_pt'],
         prefix=['event_type', 'vehicle_1_coll_pt', 'vehicle_2_coll_pt']
     )
@@ -198,48 +199,45 @@ event_dummies = (
 )
 
 atmospheric_dummies = (
-    pd.get_dummies(atmospheric_cond, columns=['atmosph_cond'], prefix=['atmosph_cond'])
+    pd.get_dummies(dataframes['atmospheric_cond'], columns=['atmosph_cond'], prefix=['atmosph_cond'])
     .groupby('accident_id').max().reset_index()
 )
 
 sub_dca_dummies = (
-    pd.get_dummies(sub_dca, columns=['sub_dca_code'], prefix=['sub_dca_code'])
+    pd.get_dummies(dataframes['sub_dca'], columns=['sub_dca_code'], prefix=['sub_dca_code'])
     .groupby('accident_id').max().reset_index()
 )
 
 road_surf_cond_dummies = (
-    pd.get_dummies(road_surface_cond, columns=['surface_cond'], prefix=['surface_cond'])
+    pd.get_dummies(dataframes['road_surface_cond'], columns=['surface_cond'], prefix=['surface_cond'])
     .groupby('accident_id').max().reset_index()
 )
 
-
-person_driver = person[person['seating_position'] == 'D']
+person_driver = dataframes['person'][dataframes['person']['seating_position'] == 'D']
 person_driver = person_driver[~person_driver.duplicated(subset=['accident_id', 'vehicle_id'], keep=False)]
 
 node_unique = (
-    node
+    dataframes['node']
     .drop('accident_id', axis=1, errors='ignore')
     .groupby('node', as_index=False)
     .agg(lambda x: x.iloc[0])
 )
 
-
 # 2.4. Structuring Data --------------------------------------------------------------------------------------
 vicroad_df = (
     vicroad_df
-    .merge(accident, on='accident_id', how='left')
-    .merge(vehicle, on=['accident_id','vehicle_id'], how='left')
+    .merge(dataframes['accident'], on='accident_id', how='left')
+    .merge(dataframes['vehicle'], on=['accident_id','vehicle_id'], how='left')
     .merge(event_dummies, on='accident_id', how='left')
     .merge(atmospheric_dummies, on='accident_id', how='left')
     .merge(sub_dca_dummies, on='accident_id', how='left')
     .merge(person_driver, on=['accident_id','vehicle_id'], how='left')
     .merge(node_unique, on='node', how='left')
     .merge(road_surf_cond_dummies, on='accident_id', how='left')
-    .merge(accident_location.drop('node', axis=1, errors='ignore'), on='accident_id', how='left')
+    .merge(dataframes['accident_location'].drop('node', axis=1, errors='ignore'), on='accident_id', how='left')
 )
 
-del [event_dummies, atmospheric_dummies, sub_dca_dummies, road_surf_cond_dummies, person_driver, node_unique,
-     accident, vehicle, accident_event, atmospheric_cond, sub_dca, person, node, road_surface_cond, accident_location]
+del [event_dummies, atmospheric_dummies, sub_dca_dummies, road_surf_cond_dummies, person_driver, node_unique]
 
 
 # 2.5. NA Cleaning -------------------------------------------------------------------------------------------
